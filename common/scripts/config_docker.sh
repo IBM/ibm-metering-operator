f890633071655a8f3c932121390bf9075c5fbbac
Steve Grube <sgrube@us.ibm.com>
2020-01-17 11:36:28 -0500

    add missing build files (#4)
    * add missing build files
    
    * add additional reviews and approvers to OWNERS file
    
    * update copyrights of scripts

--- common/scripts/config_docker.sh
+++ common/scripts/config_docker.sh
@@ -15,4 +15,13 @@
 # limitations under the License.
 #
 
-GOGC=25 golangci-lint run -c ./common/config/.golangci.yml
+KUBECTL=$(command -v kubectl)
+DOCKER_REGISTRY="quay.io"
+DOCKER_USERNAME="multicloudlab"
+DOCKER_PASSWORD=$(${KUBECTL} -n default get secret quay-cred -o jsonpath='{.data.password}' | base64 --decode)
+
+# support other container tools, e.g. podman
+CONTAINER_CLI=${CONTAINER_CLI:-docker}
+
+# login the docker registry
+${CONTAINER_CLI} login "${DOCKER_REGISTRY}" -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
--- common/scripts/install-operator-sdk.sh
+++ common/scripts/install-operator-sdk.sh
@@ -15,4 +15,11 @@
 # limitations under the License.
 #
 
-GOGC=25 golangci-lint run -c ./common/config/.golangci.yml
+echo ">>> Installing Operator SDK"
+
+# Use version 0.10.0
+RELEASE_VERSION=v0.12.0
+# Download binary
+curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
+# Install binary
+chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu && mkdir -p /usr/local/bin/ && cp operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
