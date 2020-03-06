04ef6447e41f9a284b38da769c65a1c2e2ae0d13
Curt Sherron <rcsherro@us.ibm.com>
2020-03-06 17:12:34 -0500

    fix linter errors in script

--- scripts/create-bundle.sh
+++ scripts/create-bundle.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+# Copyright 2020 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # create zip file containing the bundle to submit for Red Hat certification
 # the bundle consists of package.yaml, clusterserviceversion.yaml, crd.yaml
@@ -14,16 +28,9 @@ else
     mkdir bundle
 fi
 
-#ibm-metering-operator.package.yaml
-#ibm-metering-operator.v3.5.0.clusterserviceversion.yaml
-#operator.ibm.com_meteringmulticlouduis_crd.yaml
-#operator.ibm.com_meteringsenders_crd.yaml
-#operator.ibm.com_meterings_crd.yaml
-#operator.ibm.com_meteringuis_crd.yaml
-
 cp -p deploy/olm-catalog/ibm-metering-operator/ibm-metering-operator.package.yaml bundle/
 cp -p deploy/olm-catalog/ibm-metering-operator/3.5.0/*.yaml bundle/
 
-cd bundle
-zip ibm-metering-metadata *.yaml
+cd bundle || exit
+zip ibm-metering-metadata ./*.yaml
 cd ..
